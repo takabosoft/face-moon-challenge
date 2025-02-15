7fed0d36610cb24e9e5939a87a0f3439be5e0ec3
takabosoft <takabosoft@gmail.com>
2025-02-15 13:00:07 +0900

    インカメラの動画が出るところまで

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@
  * Release Build: npx webpack --mode=production
  */
 
-$(() => {
+$(async () => {
     console.log("OK");
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "user" }
+    });
+    const video = $(`<video class="video" autoplay playsinline>`);
+    (video[0] as HTMLVideoElement).srcObject = stream;
+    $("main").append(video);
 });
