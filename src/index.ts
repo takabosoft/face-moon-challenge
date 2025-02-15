6f175c1f1ec0cd18168028d2a8fb9186adfd411c
takabosoft <takabosoft@gmail.com>
2025-02-15 12:38:03 +0900

    httpsでスマホから接続できるところまで

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Development Build: npx webpack -w
+ * Development Server: npx live-server docs
+ * Development Server(HTTPS): npx live-server docs --https=ssl/https.js
+ * Release Build: npx webpack --mode=production
+ */
+
+$(() => {
+    console.log("OK");
+});
