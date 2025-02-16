66b8ef19e86bbd9e70cd075e46d12dc7a31e5e1d
takabosoft <takabosoft@gmail.com>
2025-02-16 16:34:03 +0900

    宇宙船を操作可能に

--- src/scenes/game/energyBar.ts
+++ src/scenes/game/energyBar.ts
@@ -1,12 +1,26 @@
 import { Component } from "../../components/component";
+import { Rgb } from "../../utils/rgb";
+import { RgbRange } from "../../utils/rgbRange";
+
+const rgbRange = new RgbRange(new Rgb(255, 0, 0), new Rgb(0, 255, 0));
 
 /** エネルギー残量表示バー */
 export class EnergyBar extends Component {
     static readonly height = 18 + 2;
     private readonly rectEl = $(`<div class="rect">`);
+    private lastRatio = -1;
 
     constructor() {
         super();
         this.element = $(`<div class="energy-bar">`).append(this.rectEl);
+    }
+
+    set ratio(ratio: number) {
+        if (this.lastRatio == ratio) { return; }
+        this.lastRatio == ratio;
+        this.rectEl.css({
+            background: rgbRange.fade(ratio),
+            width: `${ratio * 100}%`,
+        });
     }
 }
