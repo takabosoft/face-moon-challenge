d02b436b78c6b536d7da9b773fd89514dbf5c6a2
takabosoft <takabosoft@gmail.com>
2025-02-17 20:37:14 +0900

    クリアメニュー、２面

---
+++ src/data/stageInfos.ts
@@ -0,0 +1,33 @@
+import { Rect } from "../geometries/rect";
+import { Vec2 } from "../geometries/vec2";
+import { spriteInfos } from "./spriteSheet";
+
+interface StageInfo {
+    /** ステージ名 */
+    readonly title: string;
+    /** 地形テクスチャ */
+    readonly terrainRect: Rect;
+    /** 着陸場所 */
+    readonly landingZone: Rect;
+    /** 宇宙船初期値 */
+    readonly starshipPos: Vec2;
+    /** 初期エネルギー */
+    readonly energy: number;
+}
+
+export const stageInfos: readonly StageInfo[] = [
+    {
+        title: "1. かんたん",
+        terrainRect: spriteInfos.stage1Terrain,
+        landingZone: new Rect(29, 139, 38, 1),
+        starshipPos: new Vec2((spriteInfos.stage1Terrain.width - spriteInfos.spaceship.width) / 2, 16),
+        energy: 50,
+    },
+    {
+        title: "2. かんたん？",
+        terrainRect: spriteInfos.stage2Terrain,
+        landingZone: new Rect(50, 140, 38, 1),
+        starshipPos: new Vec2(32, 16),
+        energy: 50,
+    }
+];
