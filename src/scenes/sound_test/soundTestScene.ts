952cb5765febf208a95842c7307b1680a2e2911c
takabosoft <takabosoft@gmail.com>
2025-02-18 23:15:22 +0900

    ボタンタップ音

--- src/scenes/sound_test/soundTestScene.ts
+++ src/scenes/sound_test/soundTestScene.ts
@@ -14,10 +14,12 @@ export class SoundTestScene extends Scene {
             $(`<button class="button">`).text("右ガス Play").on("click", () => soundManager.rightGusSound.play()),
             $(`<button class="button">`).text("右ガス Stop").on("click", () => soundManager.rightGusSound.stop()),
 
-            $(`<button class="button">`).text("Engine Play").on("click", () => soundManager.mainEngineSound.play()),
-            $(`<button class="button">`).text("Engine Stop").on("click", () => soundManager.mainEngineSound.stop()),
+            $(`<button class="button">`).text("エンジン Play").on("click", () => soundManager.mainEngineSound.play()),
+            $(`<button class="button">`).text("エンジン Stop").on("click", () => soundManager.mainEngineSound.stop()),
 
             $(`<button class="button">`).text("爆発").on("click", () => soundManager.playExplosion()),
+
+            $(`<button class="button">`).text("ボタンタップ音").on("click", () => soundManager.playButtonTap()),
         );
     }
 }
