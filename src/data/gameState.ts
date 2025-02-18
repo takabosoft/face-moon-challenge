f643acdfd439772dcd1b46f1fb55df0ec2bbdd87
takabosoft <takabosoft@gmail.com>
2025-02-18 22:26:00 +0900

    ガス、メインエンジン効果音作成

--- src/data/gameState.ts
+++ src/data/gameState.ts
@@ -1,11 +1,15 @@
 import { stageInfos } from "./stageInfos";
 
 class GameState {
+    readonly urlParams = new URL(location.href).searchParams;
+
     stageIndex = 1;
 
     get stageInfo() {
         return stageInfos[this.stageIndex];
     }
+
+    get isSoundTest() { return this.urlParams.get("soundTest") == "1"; }
 }
 
 export const gameState = new GameState();
--- src/scenes/sound_test/soundTestScene.ts
+++ src/scenes/sound_test/soundTestScene.ts
@@ -1,11 +1,21 @@
 import { soundManager } from "../../sounds/soundManager";
 import { Scene } from "../scene";
 
+/**
+ * https://127.0.0.1:8080/?soundTest=1
+ */
 export class SoundTestScene extends Scene {
     constructor() {
         super("sound-test-scene");
         this.element.append(
-            $(`<button class="button">`).text("TEST").on("click", () => soundManager.test()),
+            $(`<button class="button">`).text("左ガス Play").on("click", () => soundManager.leftGusSound.play()),
+            $(`<button class="button">`).text("左ガス Stop").on("click", () => soundManager.leftGusSound.stop()),
+            
+            $(`<button class="button">`).text("右ガス Play").on("click", () => soundManager.rightGusSound.play()),
+            $(`<button class="button">`).text("右ガス Stop").on("click", () => soundManager.rightGusSound.stop()),
+
+            $(`<button class="button">`).text("Engine Play").on("click", () => soundManager.mainEngineSound.play()),
+            $(`<button class="button">`).text("Engine Stop").on("click", () => soundManager.mainEngineSound.stop()),
         );
     }
 }
